cf512bc33bb4611f0a86c499330e91a0b94d8d1b
Simon Pasquier <spasquie@redhat.com>
2022-05-04 13:46:32 +0200

    Revert "Revert "[bot] Bump openshift/prometheus-operator to v0.56.0""
    This reverts commit 2c9b46f3551e76870f97ba68ffbeaeb26ed945fb.
    
    Signed-off-by: Simon Pasquier <spasquie@redhat.com>

--- pkg/prometheus/statefulset.go
+++ pkg/prometheus/statefulset.go
@@ -17,6 +17,7 @@ package prometheus
 import (
 	"fmt"
 	"net/url"
+	"os"
 	"path"
 	"path/filepath"
 	"strings"
@@ -30,7 +31,10 @@ import (
 	"k8s.io/apimachinery/pkg/util/intstr"
 
 	"github.com/blang/semver/v4"
+	"github.com/go-kit/log"
+	"github.com/go-kit/log/level"
 	"github.com/pkg/errors"
+
 	monitoringv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
 	"github.com/prometheus-operator/prometheus-operator/pkg/k8sutil"
 	"github.com/prometheus-operator/prometheus-operator/pkg/operator"
@@ -342,6 +346,9 @@ func makeStatefulSetSpec(p monitoringv1.Prometheus, c *operator.Config, shard in
 		return nil, errors.Errorf("unsupported Prometheus major version %s", version)
 	}
 
+	// TODO(slashpai): Refactor the code to cover logging for all components
+	logger := log.NewLogfmtLogger(log.NewSyncWriter(os.Stderr))
+
 	promArgs := []string{
 		"-web.console.templates=/etc/prometheus/consoles",
 		"-web.console.libraries=/etc/prometheus/console_libraries",
@@ -354,7 +361,7 @@ func makeStatefulSetSpec(p monitoringv1.Prometheus, c *operator.Config, shard in
 			promArgs = append(promArgs, retentionTimeFlag+defaultRetention)
 		} else {
 			if p.Spec.Retention != "" {
-				promArgs = append(promArgs, retentionTimeFlag+p.Spec.Retention)
+				promArgs = append(promArgs, retentionTimeFlag+string(p.Spec.Retention))
 			}
 
 			if p.Spec.RetentionSize != "" {
@@ -367,7 +374,7 @@ func makeStatefulSetSpec(p monitoringv1.Prometheus, c *operator.Config, shard in
 		if p.Spec.Retention == "" {
 			promArgs = append(promArgs, retentionTimeFlag+defaultRetention)
 		} else {
-			promArgs = append(promArgs, retentionTimeFlag+p.Spec.Retention)
+			promArgs = append(promArgs, retentionTimeFlag+string(p.Spec.Retention))
 		}
 	}
 
@@ -427,6 +434,14 @@ func makeStatefulSetSpec(p monitoringv1.Prometheus, c *operator.Config, shard in
 
 	if p.Spec.EnableAdminAPI {
 		promArgs = append(promArgs, "-web.enable-admin-api")
+	}
+
+	if p.Spec.EnableRemoteWriteReceiver {
+		if version.GTE(semver.MustParse("2.33.0")) {
+			promArgs = append(promArgs, "-web.enable-remote-write-receiver")
+		} else {
+			level.Warn(logger).Log("msg", fmt.Sprintf("ignoring \"enableRemoteWriteReceiver\" not supported by Prometheus version=%s minimum_version=2.33.0", version))
+		}
 	}
 
 	if len(p.Spec.EnableFeatures) > 0 {
@@ -850,7 +865,7 @@ func makeStatefulSetSpec(p monitoringv1.Prometheus, c *operator.Config, shard in
 		}
 
 		if p.Spec.Thanos.ReadyTimeout != "" {
-			container.Args = append(container.Args, "--prometheus.ready_timeout="+p.Spec.Thanos.ReadyTimeout)
+			container.Args = append(container.Args, "--prometheus.ready_timeout="+string(p.Spec.Thanos.ReadyTimeout))
 		}
 		additionalContainers = append(additionalContainers, container)
 	}
